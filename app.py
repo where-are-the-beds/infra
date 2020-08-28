06c76f60c6bf22b6cbaaa0e70da315a649076fc0
alanaalfeche <alanaalfeche@gmail.com>
2020-08-27 23:00:51 -0500

    Deploy fargate runnign on ECS fronted by ALB

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from os import path
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs, 
+    aws_ecs_patterns as ecs_patterns,
     core,
 )
 
 class WhereAreTheBedsStack(core.Stack):
     def __init__(self, scope, name, **kwargs):
         super().__init__(scope, name, **kwargs)
+        
         vpc = ec2.Vpc(self, 'WhereAreTheBedsStack-vpc')
         cluster = ecs.Cluster(self, 'WhereAreTheBedsStack-cluster', vpc=vpc)
-        api_task_definition = ecs.FargateTaskDefinition(self, 'WhereAreTheBedsStack-api-tak', cpu=256, memory_limit_mib=512)
-        api_task_definition.add_container('api', image=ecs.ContainerImage.from_asset(path.join(path.dirname(__file__), 'api')))
+        fargate = ecs_patterns.ApplicationLoadBalancedFargateService(self, 'WhereAreTheBedsStack-fargate',cluster=cluster,
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                # image=ecs.ContainerImage.from_asset(path.join(path.dirname(__file__), 'api')),
+                image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")),
+            public_load_balancer=True)
 
 app = core.App()
 WhereAreTheBedsStack(app, 'WhereAreTheBeds')
